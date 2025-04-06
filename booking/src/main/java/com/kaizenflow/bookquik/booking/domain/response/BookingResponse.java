45c9180aad20d220027ba3dcc6119180d2ddec2e
Ahmed Marzook <m4marzook@gmail.com>
2025-04-06 23:45:52 +0100

    feat: Integrate Kafka for booking events and enhance BookingResponse with total price

--- booking/src/main/java/com/kaizenflow/bookquik/booking/domain/response/BookingResponse.java
+++ booking/src/main/java/com/kaizenflow/bookquik/booking/domain/response/BookingResponse.java
@@ -1,4 +1,6 @@
 package com.kaizenflow.bookquik.booking.domain.response;
 
+import java.math.BigDecimal;
+
 public record BookingResponse(
-        Long bookingId, Long userId, Long eventId, Long ticketCount, String ticketPrice) {}
+        Long userId, Long eventId, Long ticketCount, BigDecimal ticketPrice) {}
--- booking/src/main/java/com/kaizenflow/bookquik/booking/service/BookingService.java
+++ booking/src/main/java/com/kaizenflow/bookquik/booking/service/BookingService.java
@@ -1,28 +1,38 @@
 package com.kaizenflow.bookquik.booking.service;
 
+import java.math.BigDecimal;
+
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.kaizenflow.bookquik.booking.client.InventoryServiceClient;
 import com.kaizenflow.bookquik.booking.domain.entity.Customer;
+import com.kaizenflow.bookquik.booking.domain.event.BookingEvent;
 import com.kaizenflow.bookquik.booking.domain.request.BookingRequest;
 import com.kaizenflow.bookquik.booking.domain.response.BookingResponse;
 import com.kaizenflow.bookquik.booking.domain.response.InventoryResponse;
 import com.kaizenflow.bookquik.booking.repository.CustomerRepository;
 import com.kaizenflow.bookquik.booking.repository.OrderRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class BookingService {
     private final CustomerRepository customerRepository;
     private final OrderRepository orderRepository;
     private final InventoryServiceClient inventoryServiceClient;
+    private final KafkaTemplate<String, BookingEvent> kafkaTemplate;
 
     public BookingService(
             CustomerRepository customerRepository,
             OrderRepository orderRepository,
-            InventoryServiceClient inventoryServiceClient) {
+            InventoryServiceClient inventoryServiceClient,
+            KafkaTemplate<String, BookingEvent> kafkaTemplate) {
         this.customerRepository = customerRepository;
         this.orderRepository = orderRepository;
         this.inventoryServiceClient = inventoryServiceClient;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public BookingResponse createBooking(final BookingRequest bookingRequest) {
@@ -30,9 +40,31 @@ public class BookingService {
                 customerRepository.findById(bookingRequest.getUserId()).orElseThrow(RuntimeException::new);
         final InventoryResponse inventoryResponse =
                 inventoryServiceClient.getInventory(bookingRequest.getEventId());
-        if (inventoryResponse.totalCapacity() < bookingRequest.getTicketCount()) {
+        if (inventoryResponse.leftCapacity() < bookingRequest.getTicketCount()) {
             throw new RuntimeException("Ticket count exceeded");
         }
-        return new BookingResponse(null, null, null, null, null);
+
+        final BookingEvent bookingEvent =
+                createBookingEvent(bookingRequest, customer, inventoryResponse);
+        kafkaTemplate.send("booking", bookingEvent);
+        log.info("Booking sent to kafka: {}", bookingEvent);
+        return new BookingResponse(
+                bookingEvent.getUserId(),
+                bookingEvent.getEventId(),
+                bookingEvent.getTicketCount(),
+                bookingEvent.getTotalPrice());
+    }
+
+    private BookingEvent createBookingEvent(
+            BookingRequest bookingRequest, Customer customer, InventoryResponse inventoryResponse) {
+        return BookingEvent.builder()
+                .userId(customer.getId())
+                .eventId(bookingRequest.getEventId())
+                .ticketCount(bookingRequest.getTicketCount())
+                .totalPrice(
+                        inventoryResponse
+                                .ticketPrice()
+                                .multiply(new BigDecimal(bookingRequest.getTicketCount())))
+                .build();
     }
 }
