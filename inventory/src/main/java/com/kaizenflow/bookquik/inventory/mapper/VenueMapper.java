1117f6a8e92b83fa04639404921fa258db16e315
Ahmed Marzook <m4marzook@gmail.com>
2025-04-06 20:42:39 +0100

    feat: Add VenueMapper for mapping Venue entity to VenueInventoryResponse

--- inventory/src/main/java/com/kaizenflow/bookquik/inventory/mapper/VenueMapper.java
+++ inventory/src/main/java/com/kaizenflow/bookquik/inventory/mapper/VenueMapper.java
@@ -1,6 +1,7 @@
 package com.kaizenflow.bookquik.inventory.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import com.kaizenflow.bookquik.inventory.domain.entity.Venue;
@@ -11,5 +12,6 @@ import com.kaizenflow.bookquik.inventory.domain.response.VenueInventoryResponse;
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
 public interface VenueMapper {
 
+    @Mapping(target = "venueName", source = "name")
     VenueInventoryResponse entityToResponse(Venue venue);
 }
