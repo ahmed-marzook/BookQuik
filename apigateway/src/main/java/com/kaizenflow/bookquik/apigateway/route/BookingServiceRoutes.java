69e5b4b140583f982d8bdb7301ce699b36c974d5
Ahmed Marzook <m4marzook@gmail.com>
2025-04-24 19:24:06 +0100

    feat: Add Resilience4j circuit breaker and retry configurations to application properties

--- apigateway/src/main/java/com/kaizenflow/bookquik/apigateway/route/BookingServiceRoutes.java
+++ apigateway/src/main/java/com/kaizenflow/bookquik/apigateway/route/BookingServiceRoutes.java
@@ -1,14 +1,18 @@
 package com.kaizenflow.bookquik.apigateway.route;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 @Configuration
 public class BookingServiceRoutes {
@@ -43,7 +47,14 @@ public class BookingServiceRoutes {
                 .route(
                         RequestPredicates.GET("/api/v1/customers/email"),
                         HandlerFunctions.http(bookingServiceUrl + "/api/v1/customers/email"))
+
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("bookingServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
                 .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return GatewayRouterFunctions.route("fallbackRoute").POST("/fallbackRoute", request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).body("Booking Service is down")).build();
     }
 
     private static ServerResponse forwardWithPathVariable(ServerRequest request, String pathVariable, String baseUrl)
